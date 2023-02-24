b4c22c1b4ae235f04653fde95141aa56f90779bc
Adam Norton <adam3norton@gmail.com>
2023-02-24 13:30:07 -0700

    Add project files.

---
+++ Mission8-Group1-4/Models/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8_Group1_4.Models
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public int Quadrant { get; set; }
+
+        [Required]
+        public int CategoryId{ get; set; }
+
+        public Category Category { get; set; }
+
+        [Required]
+        public bool Completed { get; set; }
+    }
+}
---
+++ Mission8-Group1-4/Models/TaskContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8_Group1_4.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+            // blank for now
+        }
+
+        public DbSet<Task> tasks { get; set; }
+        public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Task>().HasData(
+                new Task
+                {
+                    TaskId = 1,
+                    Title = "Complete Mission 8",
+                    DueDate = new DateTime(2023, 2, 24, 23, 59, 0), // March 15th, 2023 at 11:59 PM
+                    Quadrant = 3,
+                    CategoryId = 1,
+                    Completed = false
+                }
+            );
+
+            mb.Entity<Category>().HasData(
+                new Category
+                {
+                    CategoryId = 1,
+                    Title = "Home",
+                },
+                new Category
+                {
+                    CategoryId = 2,
+                    Title = "School",
+                },
+                new Category
+                {
+                    CategoryId = 3,
+                    Title = "Work",
+                },
+                new Category
+                {
+                    CategoryId = 4,
+                    Title = "Church",
+                }
+            );
+        }
+    }
+}
