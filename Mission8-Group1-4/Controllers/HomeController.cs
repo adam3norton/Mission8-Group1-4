2b0dd8ba7f5bd6a6e3b3a18a62d98bfa23ebcd52
Parker Smith <parkerssmith0@gmail.com>
2023-02-24 21:27:44 -0700

    Edit and Delete

--- Mission8-Group1-4/Controllers/HomeController.cs
+++ Mission8-Group1-4/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace Mission8_Group1_4.Controllers
 
         private TaskContext taskContext { get; set; }
 
+        // Controller for displaying each task
         public HomeController(TaskContext somename)
         {
             taskContext = somename;
@@ -34,10 +35,54 @@ namespace Mission8_Group1_4.Controllers
         {
             var submittals = taskContext.tasks
                 .Include(i => i.Category)
+                .OrderBy(i => i.Quadrant)
                 .ToList();
 
 
             return View(submittals);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.categories = taskContext.categories.ToList();
+
+            var task = taskContext.tasks.Single(i => i.TaskId == id);
+
+            return View("Edit", task);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TaskList x)
+        {
+            if (ModelState.IsValid)
+            {
+                taskContext.Update(x);
+                taskContext.SaveChanges();
+                return RedirectToAction("Quadrants");
+            }
+            else
+            {
+                ViewBag.Categories = taskContext.categories.ToList();
+
+                return View("Edit", x);
+            }
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var task = taskContext.tasks.Single(i => i.TaskId == id);
+            return View(task);
+        }
+        [HttpPost]
+        public IActionResult Delete(TaskList x1)
+        {
+            taskContext.tasks.Remove(x1);
+            taskContext.SaveChanges();
+
+            return RedirectToAction("Quadrants");
         }
 
 
